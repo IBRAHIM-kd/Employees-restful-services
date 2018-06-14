d48a55b7bf9c11ecd0f215a989594bba22cbf5f6
Your Name <you@example.com>
2018-06-14 12:00:55 +0200

    push restful services files too

---
+++ src/main/java/com/tangentsolution/org/EMPLOYEERESTFULSERVICES/EmployeeRestfulServicesApplication.java
@@ -0,0 +1,12 @@
+package com.tangentsolution.org.EMPLOYEERESTFULSERVICES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestfulServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestfulServicesApplication.class, args);
+	}
+}
